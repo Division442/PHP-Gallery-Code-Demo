570504ea26ec623c2fe900b80e888a29cb21e995
Blake Foss <blake@division442.com>
2020-11-02 16:17:51 -0800

    Completed section 20.

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php 
         include("includes/header.php");
-        $photos = Photo::find_all();
+        //$photos = Photo::find_all();
+
+        // Pagination
+
+        $current_page = !empty($_GET['page']) ? (int)($_GET['page']) : 1;
+        $items_per_page = 2;
+        $items_total_count = Photo::count_all();
+
+        $paginate = new Paginate($current_page, $items_per_page, $items_total_count);
+
+        $sql = "SELECT * FROM photos ";
+        $sql .= "limit {$items_per_page} ";
+        $sql .= "OFFSET {$paginate->offset()} ";
+        $photos = Photo::find_by_query($sql);
         
 ?>
 
@@ -18,6 +31,32 @@
                 </div>
             <?php endforeach; ?>
         </div>
+        <!-- Pagination -->
+        <ul class="pagination justify-content-end">
+
+            <?php 
+
+                if($paginate->has_previous()) {
+                    echo "<li class='page-item'><a href='index.php?page={$paginate->previous()}'>Previous</a></li>";
+                }
+                
+                for($i = 1; $i <= $paginate->page_total() ; $i++) {
+                    if($i == $paginate->current_page) {
+                        echo "<li class='page-item active'><a href='index.php?page={$i}'>{$i}</a></li>";
+                    } else {
+                        echo "<li class='page-item'><a href='index.php?page={$i}'>{$i}</a></li>";
+                    }
+                }
+
+                if($paginate->page_total() > 1) {
+                    if($paginate->has_next()) {
+                        echo "<li class='page-item'><a href='index.php?page={$paginate->next()}'>Next</a></li>";
+                    }
+                }
+
+            ?>
+        </ul>
+
     </div>
 </div>
 <!-- /.row -->
