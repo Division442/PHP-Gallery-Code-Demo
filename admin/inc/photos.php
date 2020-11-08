518e7cfbbc3386509bbf4294227f6431ad6d01fc
Blake Foss <blake@division442.com>
2020-11-08 14:58:26 -0800

    Cleaning up deleted files.

--- admin/inc/photos.php
+++ admin/inc/photos.php
@@ -38,7 +38,7 @@
                     <td>
                         <image style="max-width: 25%; height: auto;" class="img-responsive img-rounded" src="<?php echo $photo->picture_path(); ?>">
                         <div class="pictures_link">
-                            <a href="index.php?mode=delete_photo_comment&id=<?php echo $photo->id; ?>" class="delete_link">Delete</a> | 
+                            <a href="index.php?mode=delete_photo&id=<?php echo $photo->id; ?>" class="delete_link">Delete</a> | 
                             <a href="index.php?mode=edit_photo&id=<?php echo $photo->id; ?>">Edit</a> | 
                             <a href="../photo.php?id=<?php echo $photo->id; ?>">View</a> | 
                             <a href="index.php?mode=photo_comments&id=<?php echo $photo->id; ?>">View Comments </a>
@@ -50,7 +50,7 @@
                     <td><?php echo $photo->title; ?></td>
                     <td><?php echo $photo->size; ?></td>
                     <td>
-                        <a href="photo_comment.php?id=<?php echo $photo->id; ?>">
+                        <a href="index.php?mode=photo_comments&id=<?php echo $photo->id; ?>">
                         <?php 
                             $comments = Comment::find_the_comments($photo->id);
                             echo count($comments);
