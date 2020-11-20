ace271ddc36112dffee27f4b37af69a0b2c567fb
Blake Foss <blake@division442.com>
2020-11-20 13:32:46 -0800

    -- Update config files to use production settings.

--- admin/classes/new_config.php
+++ admin/classes/new_config.php
@@ -1,9 +1,15 @@
 <?php
 
 // Database constants
+// define('DB_HOST','localhost');
+// define('DB_USER','root');
+// define('DB_PASS','root');
+// define('DB_NAME','php_course_gallery');
+
+// Production demo database settings
 define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','root');
-define('DB_NAME','php_course_gallery');
+define('DB_USER','divisio3_gallery_demo');
+define('DB_PASS','}W2pgD@62&n?');
+define('DB_NAME','divisio3_gallery_demo');
 
 ?>
--- admin/init.php
+++ admin/init.php
@@ -1,8 +1,18 @@
 <?php
 
 // /Applications/MAMP/htdocs/udemy-php-oop/gallery
+// Need to convert this from hard coded to functions to pull dynamically
+
+// Local development settings
+// defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
+// defined("SITE_ROOT") ? null : define("SITE_ROOT", DS . "Applications" . DS . "MAMP"  . DS .  "htdocs"  . DS .  "udemy-php-oop"  . DS .  "gallery" );
+// defined("INCLUDES_PATH") ? null : define("INCLUDES_PATH", SITE_ROOT.DS."admin".DS."inc");
+// defined("CLASSES_PATH") ? null : define("CLASSES_PATH", SITE_ROOT.DS."admin".DS."classes");
+
+// Production demo settings
+///home/divisio3/public_html/gallery_demo
 defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
-defined("SITE_ROOT") ? null : define("SITE_ROOT", DS . "Applications" . DS . "MAMP"  . DS .  "htdocs"  . DS .  "udemy-php-oop"  . DS .  "gallery" );
+defined("SITE_ROOT") ? null : define("SITE_ROOT", DS . "home" . DS . "divisio3"  . DS .  "public_html"  . DS .  "gallery_demo");
 defined("INCLUDES_PATH") ? null : define("INCLUDES_PATH", SITE_ROOT.DS."admin".DS."inc");
 defined("CLASSES_PATH") ? null : define("CLASSES_PATH", SITE_ROOT.DS."admin".DS."classes");
 
